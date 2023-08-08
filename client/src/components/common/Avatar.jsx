67732016199dbacb5fcd4bccabb8725cd86968d3
juniardys <juuns99@gmail.com>
2023-08-08 15:40:11 +0700

    feat: onboarding page

--- client/src/components/common/Avatar.jsx
+++ client/src/components/common/Avatar.jsx
@@ -1,7 +1,10 @@
-import React, {useState} from "react";
+import React, { useEffect, useState } from "react";
 import Image from "next/image";
-import {FaCamera} from "react-icons/fa";
+import { FaCamera } from "react-icons/fa";
 import ContextMenu from "@/components/common/ContextMenu";
+import PhotoPicker from "@/components/common/PhotoPicker";
+import PhotoLibrary from "@/components/common/PhotoLibrary";
+import CapturePhoto from "@/components/common/CapturePhoto";
 
 function Avatar({ type, image, setImage }) {
   const [hover, setHover] = useState(false);
@@ -10,53 +13,101 @@ function Avatar({ type, image, setImage }) {
     x: 0,
     y: 0,
   });
+  const [grabPhoto, setGrabPhoto] = useState(false);
+  const [showPhotoLibrary, setShowPhotoLibrary] = useState(false);
+  const [showCapturePhoto, setShowCapturePhoto] = useState(false);
 
   const showContextMenu = (e) => {
     e.preventDefault();
     setIsContextMenuVisible(true);
-    setContextMenuCoordinates({x: e.pageX, y: e.pageY});
+    setContextMenuCoordinates({ x: e.pageX, y: e.pageY });
   };
 
+  useEffect(() => {
+    if (grabPhoto) {
+      const data = document.getElementById("photo-picker");
+      data.click();
+      document.body.onfocus = (e) => {
+        setTimeout(() => {
+          setGrabPhoto(false);
+        }, 1000);
+      };
+    }
+  }, [grabPhoto]);
+
   const contextMenuOptions = [
-    { name: "Take Photo", callback: () => {}},
-    { name: "Choose From Library", callback: () => {}},
-    { name: "Upload Photo", callback: () => {}},
-    { name: "Remove Photo", callback: () => {}},
+    {
+      name: "Take Photo",
+      callback: () => {
+        setShowCapturePhoto(true);
+      },
+    },
+    {
+      name: "Choose From Library",
+      callback: () => {
+        setShowPhotoLibrary(true);
+      },
+    },
+    {
+      name: "Upload Photo",
+      callback: () => {
+        setGrabPhoto(true);
+      },
+    },
+    {
+      name: "Remove Photo",
+      callback: () => {
+        setImage("/default_avatar.png");
+      },
+    },
   ];
 
-  return <>
-    <div className="flex items-center justify-center">
-      {type === "sm" && (
+  const photoPickerChange = async (e) => {
+    const file = e.target.files[0];
+    const reader = new FileReader();
+    const data = document.createElement("img");
+    reader.onload = function (event) {
+      data.src = event.target.result;
+      data.setAttribute("data-src", event.target.result);
+    };
+    reader.readAsDataURL(file);
+    setTimeout(() => {
+      setImage(data.src);
+    }, 100);
+  };
+
+  return (
+    <>
+      <div className="flex items-center justify-center">
+        {type === "sm" && (
           <div className="relative h-10 w-10">
             <Image src={image} alt="avatar" className="rounded-full" fill />
           </div>
-      )}
-      {type === "lg" && (
+        )}
+        {type === "lg" && (
           <div className="relative h-14 w-14">
             <Image src={image} alt="avatar" className="rounded-full" fill />
           </div>
-      )}
-      {type === "xl" && (
-          <div className="relative cursor-pointer z-0"
-               onMouseEnter={() => setHover(true)}
-               onMouseLeave={() => setHover(false)}
+        )}
+        {type === "xl" && (
+          <div
+            className="relative cursor-pointer z-0"
+            onMouseEnter={() => setHover(true)}
+            onMouseLeave={() => setHover(false)}
           >
             <div
-                className={`z-10 bg-photopicker-overlay-background h-60 w-60 absolute top-0 left-0 flex items-center rounded-full justify-center flex-col text-center gap-2
+              className={`z-10 bg-photopicker-overlay-background h-60 w-60 absolute top-0 left-0 flex items-center rounded-full justify-center flex-col text-center gap-2
                 ${hover ? "visible" : "hidden"}
                 `}
-                onClick={e => showContextMenu(e)}
-                id="context-opener"
+              onClick={(e) => showContextMenu(e)}
+              id="context-opener"
             >
               <FaCamera
-                  className="text-2xl"
-                  onClick={e => showContextMenu(e)}
-                  id="context-opener"
+                className="text-2xl"
+                onClick={(e) => showContextMenu(e)}
+                id="context-opener"
               />
-              <span
-                  onClick={e => showContextMenu(e)}
-                  id="context-opener"
-              >
+              <span onClick={(e) => showContextMenu(e)} id="context-opener">
                 Change <br /> Profile <br /> Photo
               </span>
             </div>
@@ -64,17 +115,31 @@ function Avatar({ type, image, setImage }) {
               <Image src={image} alt="avatar" className="rounded-full" fill />
             </div>
           </div>
-      )}
-    </div>
-    {isContextMenuVisible && (
+        )}
+      </div>
+      {isContextMenuVisible && (
         <ContextMenu
-            options={contextMenuOptions}
-            coordinates={contextMenuCoordinates}
-            contextMenu={isContextMenuVisible}
-            setContextMenu={setIsContextMenuVisible}
+          options={contextMenuOptions}
+          coordinates={contextMenuCoordinates}
+          contextMenu={isContextMenuVisible}
+          setContextMenu={setIsContextMenuVisible}
         />
-    )}
-  </>;
+      )}
+      {showCapturePhoto && (
+        <CapturePhoto
+          setImage={setImage}
+          hide={() => setShowCapturePhoto(false)}
+        />
+      )}
+      {showPhotoLibrary && (
+        <PhotoLibrary
+          setImage={setImage}
+          hidePhotoLibrary={() => setShowPhotoLibrary(false)}
+        />
+      )}
+      {grabPhoto && <PhotoPicker onChange={photoPickerChange} />}
+    </>
+  );
 }
 
 export default Avatar;
